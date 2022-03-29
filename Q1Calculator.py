5e8d15995fb8c471bb57e9edf02ddbb08ae7a732
Esther <98366701+2102069esthertsw@users.noreply.github.com>
2022-03-29 11:54:10 +0800

    Update Q1Calculator.py

--- Q1Calculator.py
+++ Q1Calculator.py
@@ -1,5 +1,7 @@
 class Calculator:
     answer = 0
+    
+#     reset answer value
     def clear(self):
         self.answer = 0
     
@@ -18,13 +20,18 @@ class Calculator:
     def divider(self, input1, input2):
         self.answer = input1 / input2
         print("Quotient: ",self.answer)
-    
+   
+# instantiate Calculator class
 myCalc = Calculator()
+
+# prompt user for 2 numbers
 input1 = int(input("Enter the 1st number: "))
 input2 = int(input("Enter the 2nd number: "))
+# perform calculations using the numbers
 myCalc.adder(input1,input2)
 myCalc.subtractor(input1,input2)
 myCalc.multiplier(input1,input2)
 myCalc.divider(input1,input2)
+# clear after performing functions
 myCalc.clear()
 print("After clearing: answer = ", myCalc.answer)
